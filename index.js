5160e53acc2f8bc2928a53ca7027fb076a4abce4
kliptak2 <91429418+kliptak2@users.noreply.github.com>
2022-06-16 15:08:30 -0400

    create li element and button

--- index.js
+++ index.js
@@ -4,18 +4,54 @@ var listEl = document.getElementById("todo-list")
 
 // capture user inputs                       
 form.onsubmit = function(capture) {
-    capture.preventDefault()            // Supposed to use but not working. Removes default html form submission behavior. Alt return false
-
+    // prevent default html form submission behavior (Alt: use return false at end of function)
+    capture.preventDefault()
+    var data = document.querySelector("input").value   // could I use form.value?
+    console.log(data)
+    // generate li element
+    var li = document.createElement("li");
+    // append li to ul#todo-list
+    listEl.appendChild(li);
+    // generate button & put it in li element
+    var btn = document.createElement("button");
+    btn.innerHTML = data;
+    li.appendChild(button);
 }
 
 // execute a function when user clicks on list items
 function listClicks(){
-                          // add strikethrough when list item is clicked
-                          // if list item has a strikethrough when clicked, remove it from list 
+// add strikethrough when list item is clicked. Use onclick event
+// if list item has a strikethrough when clicked, remove it from list
+
 }
 
-// make user input appear in list using appendChild(). Don't need to hold list items in an array bc we're appending the DOM
-// form.children - gives an array of children
-// .value
-// look up javascript createElement
-// look up javascript button add.onclick
+
+
+
+//*************** To Do *********************
+// look up form.children (gives an array of children)
+
+
+// NOTES
+
+// Clear out data - Manual Way
+// form.onsubmit = function (e){
+//     e.preventDefault()
+//     var input = document.querySelector("input")
+//     console.log(input.value)
+//     input.value = ""
+// }
+
+// Clear out data - Simpler Way (*if html is written correctly)
+// form.onsubmit = function (e){
+//     e.preventDefault()
+//     var data = this.firstName.value
+//     console.log(data)
+//     this.firstNAme.value = ""
+// }
+
+
+// How to make a button that displays the captured data
+// const btn = document.createElement("button");
+// btn.innerHTML = data;
+// document.body.appendChild(btn);
