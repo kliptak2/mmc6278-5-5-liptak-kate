f17e1f1ec158e6673014aace834bff5e4047e6f1
kliptak2 <91429418+kliptak2@users.noreply.github.com>
2022-06-16 13:24:48 -0400

    delete unneeded code

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 // list global variables
-var inputField = ""
-var inputFieldEl = document.getElementById("add-todo")
-var listField = ""
-var listFieldEl = document.getElementById("todo-list")
+var form = document.getElementById("add-todo")
+var listEl = document.getElementById("todo-list")
 
-// make a function for actions that need to happen when page loads
-function onPageLoad(){
-    inputFieldEl.innerHTML = inputField
-    listFieldEl.innerHTML = listField
-}
+// capture user inputs                       
+form.onsubmit = function(capture) {
+    capture.preventDefault()            // Supposed to use but not working. Removes default html form submission behavior. Alt return false
 
-// execute a function when user presses Enter key or Add button
-var submit = document.getElementById("add-todo");
-submit.addEventListener("keypress", function(addItem){     // or form.onsubmit = function(event){} ?
-    if (addItem.key === "Enter"){
-        addItem.preventDefault();                       // removes default html form submission behavior
-        document.getElementById("button").click();
-    }
-});
-
-// capture user inputs
-var form = document.querySelector("form")
-form.onsubmit = function(event) {            // attach an event to the form with an anonymous function
-    e.preventDefault()                      
-    var listItem = event.listItem//something else           // make user input appear in list / appendChild(). Don't need to hold list items in an array bc we're appending the DOM
 }
 
 // execute a function when user clicks on list items
@@ -32,3 +14,8 @@ function listClicks(){
                           // if list item has a strikethrough when clicked, remove it from list 
 }
 
+// make user input appear in list using appendChild(). Don't need to hold list items in an array bc we're appending the DOM
+// form.children - gives an array of children
+// .value
+// look up javascript createElement
+// look up javascript button add.onclick
