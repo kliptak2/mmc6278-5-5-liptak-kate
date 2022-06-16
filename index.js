eece375fc7dde398fdf2b26a99b230a4512ca00b
Kate Liptak <kate.liptak@gmail.com>
2022-06-15 23:24:31 -0400

    pseudocode

---
+++ index.js
@@ -0,0 +1,12 @@
+// list global variables
+
+// make a function for actions that need to happen when page loads
+// make list and input fields empty
+
+// make an anonymous function to capture user inputs
+// make a key event for Enter key or Add button
+// make user input appear in list
+// add strikethrough when list item is clicked
+// if list item has a strikethrough when clicked, remove it from list 
+
+// remove default html form behavior
