df983638b770f32a016f8520ac7b0be1b9b04265
Kate Liptak <kate.liptak@gmail.com>
2022-06-16 01:27:40 -0400

    sketch out functions

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 // list global variables
+var inputField = ""
+var inputFieldEl = document.getElementById("add-todo")
+var listField = ""
+var listFieldEl = document.getElementById("todo-list")
 
 // make a function for actions that need to happen when page loads
-// make list and input fields empty
+function onPageLoad(){
+    inputFieldEl.innerHTML = inputField
+    listFieldEl.innerHTML = listField
+}
 
-// make an anonymous function to capture user inputs
-// make a key event for Enter key or Add button
-// make user input appear in list
-// add strikethrough when list item is clicked
-// if list item has a strikethrough when clicked, remove it from list 
+// execute a function when user presses Enter key or Add button
+var submit = document.getElementById("add-todo");
+submit.addEventListener("keypress", function(addItem){     // or form.onsubmit = function(event){} ?
+    if (addItem.key === "Enter"){
+        addItem.preventDefault();                       // removes default html form submission behavior
+        document.getElementById("button").click();
+    }
+});
 
-// remove default html form behavior
+// capture user inputs
+var form = document.querySelector("form")
+form.onsubmit = function(event) {            // attach an event to the form with an anonymous function
+    e.preventDefault()                      
+    var listItem = event.listItem//something else           // make user input appear in list / appendChild(). Don't need to hold list items in an array bc we're appending the DOM
+}
+
+// execute a function when user clicks on list items
+function listClicks(){
+                          // add strikethrough when list item is clicked
+                          // if list item has a strikethrough when clicked, remove it from list 
+}
+
